450042e723fb2520c180d6c4e206951069761ae1
linnypham <89488749+linnypham@users.noreply.github.com>
2024-01-31 12:16:48 -0500

    update fib

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,12 @@
+fib = {0:0, 1:1}
 def fibonacci(n):
-    if n in {0,1}:
-        return n
+    if n in fib:
+        return fib[n]
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        fib[n] = fibonacci(n-1) + fibonacci(n-2)
+        return fib[n]
 
 a = int(input('The fibonacci number at location: '))
 print(f'Fibonacci number: {fibonacci(a-1)}')
+print(fib)
 
