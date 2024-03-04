f776b6624fea1d7c79ecd55793d7f3a8f1471a87
linnypham <89488749+linnypham@users.noreply.github.com>
2024-03-04 18:18:23 -0500

    class

---
+++ lab/a2.py
@@ -0,0 +1,12 @@
+int a
+int b
+
+def calculator():
+    operand = []
+    operators = []
+    while operators.peak():
+        if operand.peak()>=operators.push():
+
+
+input = "10 * 2 - 15"
+print(output)
