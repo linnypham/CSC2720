3e9114863ff4fe60ee74ddcde3584aed5780b3d8
linnypham <89488749+linnypham@users.noreply.github.com>
2024-03-05 12:12:09 -0500

    a2

--- lab/a2.py
+++ lab/a2.py
@@ -1,12 +1,39 @@
-int a
-int b
-
-def calculator():
-    operand = []
-    operators = []
-    while operators.peak():
-        if operand.peak()>=operators.push():
-
+def calculator(input):
+    def calculate(operators, operand):
+        operator = operators.pop()
+        a = operand.pop()
+        b = operand.pop()
+        if operator == '+':
+            operand.append(b+a)
+        elif operator == '-':
+            operand.append(b-a)
+        elif operator == '*':
+            operand.append(b*a)
+        elif operator == '/':
+            operand.append(b/a)
+    def priority(operator):
+        if operator == '+' or operator == '-':
+            return 1
+        elif operator == '*' or operator == '/':
+            return 2
+        else:
+            return 0
+    def evaluate(input):
+        equation = input.split()
+        operand = []
+        operators = []
+        i=0
+        while i < len(equation):
+            if equation[i] == ' ':
+                i+=1
+            if equation[i].isdigit():
+                operand.append((int(equation[i])))
+            else:
+                operators.append(equation[i])
+            while operators[-1]:
+                if operand[-1]>=operators[-1]:
+                    pass
 
 input = "10 * 2 - 15"
-print(output)
+equation = input.split()
+print(equation)
