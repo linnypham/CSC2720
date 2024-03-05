2d6ee2baec2092dded6921e93d01a2c84c3dfc5d
linnypham <89488749+linnypham@users.noreply.github.com>
2024-03-05 12:59:47 -0500

    a2

--- lab/a2.py
+++ lab/a2.py
@@ -1,39 +1,55 @@
 def calculator(input):
-    def calculate(operators, operand):
-        operator = operators.pop()
-        a = operand.pop()
-        b = operand.pop()
-        if operator == '+':
-            operand.append(b+a)
+    def calculate(operators, operand): #calculation
+        operator = operators.pop() #pop the operator
+        b = operand.pop() #pop the last number
+        a = operand.pop() #pop the next number
+        if operator == '+': #do the calculation base on the operator
+            operand.append(a + b)
         elif operator == '-':
-            operand.append(b-a)
+            operand.append(a - b)
         elif operator == '*':
-            operand.append(b*a)
+            operand.append(a * b)
         elif operator == '/':
-            operand.append(b/a)
-    def priority(operator):
+            operand.append(a / b)
+
+    def priority(operator): #prioritize doing * and / first
         if operator == '+' or operator == '-':
             return 1
         elif operator == '*' or operator == '/':
             return 2
         else:
             return 0
+
     def evaluate(input):
-        equation = input.split()
-        operand = []
-        operators = []
-        i=0
+        equation = input.split() #split input into strings
+        operand = [] #store digit
+        operators = [] #store operators
+        i = 0 #counters
         while i < len(equation):
             if equation[i] == ' ':
-                i+=1
-            if equation[i].isdigit():
-                operand.append((int(equation[i])))
-            else:
-                operators.append(equation[i])
-            while operators[-1]:
-                if operand[-1]>=operators[-1]:
-                    pass
+                i += 1
+                continue
+            if equation[i].isdigit(): #append digit into operand
+                operand.append(int(equation[i]))
+            elif equation[i] in '+-*/': #check operators
+                while (len(operators) > 0 and priority(operators[-1]) >= priority(equation[i])):#calculating base on the priority of the operators
+                    calculate(operators, operand)
+                operators.append(equation[i]) #append operator into operators
+            i += 1
 
+        while len(operators) > 0:
+            calculate(operators, operand)
+
+        return operand[0]
+
+    result = evaluate(input)
+    return result
+
+# Example usage:
 input = "10 * 2 - 15"
-equation = input.split()
-print(equation)
+output = calculator(input)
+print(output)
+
+'''Since deque is similar to a double linked-list, 
+you can traverse through the list, appending, and popping  from both sides instead of 1 side.
+So deque will be faster in Time complexity'''
