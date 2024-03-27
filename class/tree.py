5ec5b03c209cb455298c0aeb1d9189c4fec422e7
linnypham <89488749+linnypham@users.noreply.github.com>
2024-03-27 12:21:25 -0400

    bi tree

--- class/tree.py
+++ class/tree.py
@@ -17,29 +17,31 @@ def inOrder(root):#recursive
     inOrder(root)
     return list
 '''
-def inOrder(root):  #interative
-    IOT = []
-    cs = deque()
-    current = root
-    while current or len(cs) > 0:
-        if current:
-            cs.append(current)
-            current = current.left
-        else:
-            current = cs.pop()
-            IOT.append(current.val)
-            current = current.right
-    return IOT
+    def inOrder(root):  #interative
+        IOT = []
+        cs = deque()
+        current = root
+        while current or len(cs) > 0:
+            if current:
+                cs.append(current)
+                current = current.left
+            else:
+                current = cs.pop()
+                IOT.append(current.val)
+                current = current.right
+        return IOT
 
-def levelOrder(root):
-    LOT = []
-    q = deque()
-    q.append(root)
-    while q:
-        a_node = q.popleft()
-        LOT.append(a_node.val)
-        if a_node.left:
-            q.append(a_node.left)
-        if a_node.right:
-            q.append(a_node.right)
-    return LOT
+    def levelOrder(root):
+        LOT = []
+        q = deque()
+        q.append(root)
+        while q:
+            a_node = q.popleft()
+            LOT.append(a_node.val)
+            if a_node.left:
+                q.append(a_node.left)
+            if a_node.right:
+                q.append(a_node.right)
+        return LOT
+
+root = TreeNode(1)
