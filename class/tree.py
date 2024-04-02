f1d8ce3bab3b8478f35f8534577a92780a135cc2
linnypham <89488749+linnypham@users.noreply.github.com>
2024-04-01 20:39:54 -0400

    a3

--- class/tree.py
+++ class/tree.py
@@ -5,43 +5,43 @@ class TreeNode:
         self.right = right
         self.val = val
 
-    '''
-    def inOrder(self,root):#recursive
-        list = []
-        if root == None:
-            return
+'''
+def inOrder(root):#recursive
+    list = []
+    if root == None:
+        return
+    else:
+        inOrder(root.left)
+        list.append(root.val)
+        inOrder(root.right)
+    return list
+'''
+def inOrder(root):  #interative
+    IOT = []
+    cs = deque()
+    current = root
+    while current or len(cs) > 0:
+        if current:
+            cs.append(current)
+            current = current.left
         else:
-            inOrder(root.left)
-            list.append(root.val)
-            inOrder(root.right)
-        return list
-    '''
-    def inOrder(self,root):  #interative
-        IOT = []
-        cs = deque()
-        current = root
-        while current or len(cs) > 0:
-            if current:
-                cs.append(current)
-                current = current.left
-            else:
-                current = cs.pop()
-                IOT.append(current.val)
-                current = current.right
-        return IOT
+            current = cs.pop()
+            IOT.append(current.val)
+            current = current.right
+    return IOT
 
-    def levelOrder(self,root):
-        LOT = []
-        q = deque()
-        q.append(root)
-        while q:
-            a_node = q.popleft()
-            LOT.append(a_node.val)
-            if a_node.left:
-                q.append(a_node.left)
-            if a_node.right:
-                q.append(a_node.right)
-        return LOT
+def levelOrder(root):
+    LOT = []
+    q = deque()
+    q.append(root)
+    while q:
+        a_node = q.popleft()
+        LOT.append(a_node.val)
+        if a_node.left:
+            q.append(a_node.left)
+        if a_node.right:
+            q.append(a_node.right)
+    return LOT
 
 root = TreeNode(1)
 root.left = TreeNode(2)
---
+++ lab/a3.py
@@ -0,0 +1,72 @@
+from collections import deque
+class Node:
+# A function to create a new node
+    def __init__(self, key):
+        self.data = key
+        self.left = None
+        self.right = None
+def inOrder(root): #In-order traversal
+    IOT = [] #traversal list
+    cs = deque() #call stack
+    current = root
+    while current or len(cs) > 0:
+        if current:
+            cs.append(current) #append value in call stack
+            current = current.left # move left
+        else:
+            current = cs.pop() #pop value in call stack
+            IOT.append(current.data) #appen vallue in IOT list
+            current = current.right # move right
+    return IOT
+def smallestNode(root): #find the node with lowest value
+    current = root
+    while current.left: #keep  going the the left of left subtree to find smallest value
+        current = current.left
+    return current
+def deleteRoot(root):
+    newNode = smallestNode(root.right) #find the smallest value of right subtree of the root
+    root.data = newNode.data #replace root value with new value
+    newNode.data = None #replace the value of smallest Node to Null
+
+root = Node(4)
+root.left = Node(2)
+root.right = Node(6)
+root.left.left = Node(1)
+root.left.right = Node(3)
+root.right.left = Node(5)
+root.right.right = Node(7)
+deleteRoot(root)
+output = inOrder(root)
+print(output)
+
+'''test1: Node is null
+root = Node()
+root.left = Node()
+root.right = Node()
+deleteRoot(root)
+output = levelOrder(root)
+print(output)
+
+test2: Node is string
+root = Node('a')
+root.left = Node('b')
+root.right = Node('c')
+deleteRoot(root)
+output = levelOrder(root)
+print(output)
+
+test3: Node is string and int
+root = Node('a')
+root.left = Node(3)
+root.right = Node('c')
+deleteRoot(root)
+output = levelOrder(root)
+print(output)
+
+test4: Node is Null and String and Int
+root = Node('a')
+root.left = Node(3)
+root.right = Node()
+deleteRoot(root)
+output = levelOrder(root)
+print(output)'''
