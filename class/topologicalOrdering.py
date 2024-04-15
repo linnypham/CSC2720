5a20df7365f166f5b8ced13e3b9a5212c37dc7e2
linnypham <89488749+linnypham@users.noreply.github.com>
2024-04-15 16:57:00 -0400

    TO

---
+++ class/topologicalOrdering.py
@@ -0,0 +1,33 @@
+#time: _(V+E)
+#space: visiting, visited, call stack
+TO = [2,3,1,0,4,5]
+PRL=[[0,1],[0,2],[1,3],[3,2],[4,0],[5,0]]
+n = 6
+
+AL = {i:[] for i in range(n)} # adjacency lists
+for i,j in PRL:
+    AL[i].append(j)
+visiting = set()
+visited = set()
+TO = []
+def dfs(crs):
+    if crs in visited:
+        return False
+    if crs in visited:
+        return True
+    visiting.add(crs)
+    for i in AL[crs]:
+        if dfs(i)==False:
+            return False
+    visiting.remove(crs)
+    visited.add(crs)
+    TO.append(crs)
+    return True
+
+for c in range(n):
+    if dfs(c)==False:
+        return[]
+return TO
+
+
+
